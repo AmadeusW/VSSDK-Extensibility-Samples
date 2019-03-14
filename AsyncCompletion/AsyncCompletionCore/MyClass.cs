f301234ad2e7be049aed562e59165b2d9675d278
Amadeusz Wieczorek <amwieczo@microsoft.com>
2019-03-13 17:04:37 -0700

    adds VS:mac addin

---
+++ AsyncCompletion/AsyncCompletionCore/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AsyncCompletionCore
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ AsyncCompletion/VSMacAddin/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "AsyncCompletionSample",
+    Namespace = "AsyncCompletionSample",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("AsyncCompletionSample")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Demonstrates xPlat Async Completion in Visual Studio")]
+[assembly: AddinAuthor("Amadeusz Wieczorek")]
